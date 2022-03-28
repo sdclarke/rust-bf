c76a40fecbb15141d132268db47b569b2754d904
Scott Clarke <scott.clarke@codethink.co.uk>
2022-03-28 16:21:51 +0100

    Adapt program for week 2 2nd homework

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,119 @@
 use std::env::args;
 use std::error::Error;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs;
+
+#[derive(Debug, Clone, Copy)]
+enum RawInstruction {
+    Increment,
+    Decrement,
+    LoopOpen,
+    LoopClose,
+    RightShift,
+    LeftShift,
+    Output,
+    Input,
+}
+
+impl RawInstruction {
+    fn from_char(c: char) -> Option<RawInstruction> {
+        match c {
+            '+' => Some(Self::Increment),
+            '-' => Some(Self::Decrement),
+            '[' => Some(Self::LoopOpen),
+            ']' => Some(Self::LoopClose),
+            '>' => Some(Self::RightShift),
+            '<' => Some(Self::LeftShift),
+            '.' => Some(Self::Output),
+            ',' => Some(Self::Input),
+            _ => None,
+        }
+    }
+}
+
+impl std::fmt::Display for RawInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Increment => write!(f, "Increment value at pointer"),
+            Self::Decrement => write!(f, "Decrement value at pointer"),
+            Self::LoopOpen => write!(f, "Start looping"),
+            Self::LoopClose => write!(f, "Stop looping"),
+            Self::RightShift => write!(f, "Increment pointer"),
+            Self::LeftShift => write!(f, "Decrement pointer"),
+            Self::Output => write!(f, "Output byte at pointer"),
+            Self::Input => write!(f, "Read byte of input to location at pointer"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct RawInstructionStruct {
+    raw_instruction: RawInstruction,
+    line: i64,
+    column: i64,
+}
+
+impl RawInstructionStruct {
+    fn new(raw_instruction: RawInstruction, line: i64, column: i64) -> Self {
+        Self {
+            raw_instruction,
+            line,
+            column,
+        }
+    }
+
+    fn raw_instruction(&self) -> RawInstruction {
+        self.raw_instruction
+    }
+
+    fn line(&self) -> i64 {
+        self.line
+    }
+
+    fn column(&self) -> i64 {
+        self.column
+    }
+}
+
+fn parse_file(filename: &str) -> Result<Vec<RawInstructionStruct>, Box<dyn Error>> {
+    let contents = fs::read_to_string(filename)?;
+    let bf = contents.trim_end().split('\n');
+
+    let mut raw_instruction_vec = Vec::new();
+    let mut line = 1;
+    for s in bf {
+        let mut column = 1;
+        // For each character in a line
+        for c in s.chars() {
+            // If it is a bf char, create struct and add to Vec
+            let raw_instruction = match RawInstruction::from_char(c) {
+                Some(r) => r,
+                None => {
+                    // If not bf char, skip
+                    column += 1;
+                    continue;
+                }
+            };
+            raw_instruction_vec.push(RawInstructionStruct::new(raw_instruction, line, column));
+            column += 1;
+        }
+        line += 1;
+    }
+    Ok(raw_instruction_vec)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Get the filename which will be the second element of the args
     let filename = args().nth(1).ok_or("Expected filename")?;
-    // Open the file
-    let file = File::open(filename)?;
-    // Fail early if the file is empty
-    if file.metadata()?.len() == 0 {
-        return Err("File is empty".into());
+
+    for r in parse_file(&filename)? {
+        println!(
+            "[{}:{}:{}] {}",
+            filename,
+            r.line(),
+            r.column(),
+            r.raw_instruction()
+        );
     }
-    // Wrap the file in a BufReader
-    let file = BufReader::new(file);
-    // A list of the characters we care about
-    let bf_chars = ['+', '-', '<', '>', '.', ',', '[', ']'];
-
-    let bf = file
-        .lines()
-        .map(|l| {
-            // Map Ok value of Result
-            l.map(|mut s| {
-                // Remove any non-bf chars
-                s.retain(|c| bf_chars.contains(&c));
-                s
-            })
-        })
-        .collect::<Result<Vec<String>, std::io::Error>>()?; // Collect as Result and check the error
-
-    // I was using the below lines at one stage to produce and iterator for a for loop to print
-    // each line of the bf program
-    // I prefer the new solution for future proofing and also the fact that there is no for loop
-    // needed
-    // (These lines would've come after the collect() in case it isn't clear)
-    //.iter() // Create an iterator which filters blank lines
-    //.filter(|s| !s.is_empty())
-
-    // Get the resulting bf program as a vector of chars (in prep for future)
-    let bf: Vec<char> = bf.iter().flat_map(|s| s.chars()).collect();
-    // Is this potentially a bad idea if the resulting bf program is massive? Presumably not really
-    // any more troublesome than having the above Vec<char>
-    println!("{}", bf.iter().collect::<String>());
-
-    /* This is the first solution I came up with before messing with map()/collect()/filter()
-     * These lines would replace the above for loop
-    // For each line
-    for line in file.lines() {
-        let mut line = line?;
-        // Keep only the characters in the list
-        line.retain(|c| bf_chars.contains(&c));
-        // Drop empty lines
-        if !line.is_empty() {
-            print!("{}", line);
-        }
-    }
-    */
 
     Ok(())
 }
